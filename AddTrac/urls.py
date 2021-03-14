6c239f9a625a262ed91d194ea3aedcf4e7418ebb
Additya Singhal <addityadd@gmail.com>
2021-03-14 19:55:03 +0530

    Serving static files

--- AddTrac/urls.py
+++ AddTrac/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 urlpatterns = [
@@ -24,3 +24,4 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
